2163c0f464bb725132fd7f36246be95940edd8df
Abeed <abnazar@ucsc.edu>
2024-08-13 19:21:02 -0700

    updated RGB565_combiner and icons

--- Gauge_Icons/RGB565_combiner.py
+++ Gauge_Icons/RGB565_combiner.py
@@ -9,9 +9,12 @@
        0x80C0,   0xB960,   0xD9C0,   0xE9E0
   This is needed to use the Adafruit GFX library bitmap drawing functions
   as the Waveshare library uses the 8 bit format
+
+  Changelog:
+  8/12/2024 - Authored file
+  8/13/2024 - Simplified parsing of input file, corrected output array # of elements, and
+              removed unnecessary header comment from output file
 '''
-
-
 
 # shuffleBytes()
 # Shuffles the last two items in a list
@@ -33,6 +36,9 @@ filesList = ["digit_0.c", "digit_1.c", "digit_2.c", "digit_3.c", "digit_4.c",
 "index_12on.c", "index_8off.c", "index_8on.c", "index_9off.c", "index_9on.c", "text_block.c", "unit_PSI.c", 
 "unit_celcius.c"]
 
+# For testing one file at a time
+# filesList = ["digit_9.c"]
+
 for fileInd in range(0, len(filesList)):
 
   # generate a new filename with "_new" to write to
@@ -42,61 +48,60 @@ for fileInd in range(0, len(filesList)):
   outputFileName_temp.insert(2, ".")
   outputFileName = "".join(outputFileName_temp)
 
-  lettersFile = open(lettersFileName, "r")
-  # outputFile = open("digit_0_adj.c", "w")
+  with open(lettersFileName, "r") as lettersFile:
+    # outputFile = open("digit_0_adj.c", "w")
 
-  parsedBytes = []
+    parsedBytes = []
 
-  for lineNum, currLine in enumerate(lettersFile):
-    # skip the first line of the file
-    if lineNum == 0:
-      continue
-    # print(str(lineNum) + ": " + line + "\n")
-    
-    # This loop goes through each line and finds each hex value
-    # Then each pair of 8-bit chars is joined into a 16-bit int
-    for letterNum, currLetter in enumerate(currLine):
+    for lineNum, currLine in enumerate(lettersFile):
+      # skip the first line of the file
+      if lineNum == 0:
+        continue
+      # print(str(lineNum) + ": " + line + "\n")
       
-      # Store the first letter/digit 
-      if ((letterNum + 3) % 5) == 0: 
-        # print("Letter 1 " + str(currLetter) + ": " + currLetter)
-        Letter1 = currLetter
+      currByte = ""
+      for letter in currLine:
+        # Store each letter inbetween commas into a string
+        if letter != ",":
+          currByte = currByte + letter
+        # Convert the string into int and store
+        else:
+          currByte = int(currByte, 16)
+          parsedBytes.append(currByte)
+          # switch the order of two bytes when a full int is recorded
+          if len(parsedBytes) % 2 == 0:
+            parsedBytes = shuffleBytes(parsedBytes)
+          currByte = ""
 
-      # Store the second letter/digit
-      if ((letterNum + 2) % 5) == 0: 
-        # print("Letter 2 " + str(currLetter) + ": " + currLetter)
-        Letter2 = currLetter
-        # Store the full 8-bit value
-        currCharStr = Letter1 + Letter2
-        currChar = int(currCharStr, 16)
-        # print(currChar)
-        parsedBytes.append(currChar)
-
-        # Shuffle every pair of bytes into the correct order for RGB565
-        if (len(parsedBytes) % 2 == 0):
-          shuffleBytes(parsedBytes)
-
-  lettersFile.close()
 
   # print(parsedBytes)
   RGB565_Bytes = []
-  for i in range(0, len(parsedBytes)-2, 2):
+  for i in range(0, len(parsedBytes)-1, 2):
     # print(hex(parsedBytes[i]), hex(parsedBytes[i+1]), end=",")
     curr565 = parsedBytes[i] << 8 | parsedBytes[i+1]
     RGB565_Bytes.append(curr565)
-    # print(hex(curr565).upper(), end=",")
+    # print("{0:#0{1}X}".format(curr565,6), end=",")
 
   newSize = len(RGB565_Bytes)
+  # print(newSize)
+
 
   with open(lettersFileName, 'r') as lettersFile, open(outputFileName, 'w') as outputFile:
-    # Reuse the first line of the file, except the array size and data type must change
+    # Reuse the first line of the file, except the following changes:
+    # array size must change
     firstLine = lettersFile.readline()
     sizeStartInd = firstLine.index("[") + 1
     sizeEndInd = firstLine.index("]")
     oldSizeStr = firstLine[sizeStartInd:sizeEndInd]
 
+    # data type must change
     firstLine = firstLine.replace(oldSizeStr, str(newSize))
     firstLine = firstLine.replace("unsigned char", "uint16_t")
+
+    # and the commented header row can be deleted
+    headerInd = firstLine.find("{") + 1
+    firstLine = firstLine.replace(firstLine[headerInd:], "\n")
+
     outputFile.write(firstLine)
 
     i = 0
